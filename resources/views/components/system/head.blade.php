f9621238f868e2b40aecddfd9f58113270418b5d
kasperfci <kasperfci@gmail.com>
2021-11-04 18:38:17 +0200

    small updates

--- resources/views/components/system/head.blade.php
+++ resources/views/components/system/head.blade.php
@@ -1,22 +1,44 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Jumia - @yield('title')</title>
+  <title>Phone Validator - @yield('title')</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{asset('/assets/system/plugins/fontawesome-free/css/all.min.css')}}">
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- DataTables -->
   <link rel="stylesheet" href="{{asset('/assets/system/plugins/datatables-bs4/css/dataTables.bootstrap4.css')}}">
- 
+  <link rel="stylesheet" href="{{asset('/assets/system/plugins/summernote/summernote-bs4.css')}}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{asset('/assets/system/dist/css/adminlte.min.css')}}">
   <!-- Google Font: Source Sans Pro -->
+  <link rel="shortcut icon" href="{{asset('/assets/images/others/favicon.png')}}" />
+
+  <!-- Ionicons -->
+  <!-- daterange picker -->
+  <link rel="stylesheet" href="{{asset('/assets/system/plugins/daterangepicker/daterangepicker.css')}}">
+  <!-- iCheck for checkboxes and radio inputs -->
+  <link rel="stylesheet" href="{{asset('/assets/system/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+
+  <link rel="stylesheet" href="{{asset('/assets/system/plugins/summernote/summernote-bs4.css')}}">
+  <!-- Bootstrap Color Picker -->
+  <link rel="stylesheet" href="{{asset('/assets/system/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css')}}">
+  <!-- Tempusdominus Bbootstrap 4 -->
+  <link rel="stylesheet" href="{{asset('/assets/system/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+  <!-- Select2 -->
+  <link rel="stylesheet" href="{{asset('/assets/system/plugins/select2/css/select2.min.css')}}">
+  <link rel="stylesheet" href="{{asset('/assets/system/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css')}}">
+  <!-- Bootstrap4 Duallistbox -->
+  <link rel="stylesheet" href="{{asset('/assets/system/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css')}}">
+  <!-- Theme style -->
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
   <link rel="stylesheet" href="{{asset('/assets/system/custom.css')}}">
-
+ 
      @yield('pagecss')
 </head>
--- resources/views/components/system/script.blade.php
+++ resources/views/components/system/script.blade.php
@@ -1,8 +1,23 @@
 <script src="{{asset('/assets/system/plugins/jquery/jquery.min.js')}}"></script>
 <!-- Bootstrap 4 -->
+<script src="{{asset('/assets/system/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- DataTables -->
 <script src="{{asset('/assets/system/plugins/datatables/jquery.dataTables.js')}}"></script>
 <script src="{{asset('/assets/system/plugins/datatables-bs4/js/dataTables.bootstrap4.js')}}"></script>
 <!-- AdminLTE App -->
+<script src="{{asset('/assets/system/dist/js/adminlte.min.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{asset('/assets/system/dist/js/demo.js')}}"></script>
+<!-- Select2 -->
+<script src="{{asset('/assets/system/plugins/select2/js/select2.full.min.js')}}"></script>
+<!-- Bootstrap4 Duallistbox -->
+<script src="{{asset('/assets/system/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js')}}"></script>
+<!-- InputMask -->
+<script src="{{asset('/assets/system/plugins/inputmask/jquery.inputmask.bundle.js')}}"></script>
+<script src="{{asset('/assets/system/plugins/moment/moment.min.js')}}"></script>
+<!-- date-range-picker -->
+
+<script src="{{asset('/assets/system/plugins/daterangepicker/daterangepicker.js')}}"></script>
+<!-- bootstrap color picker -->
 
 @yield('pagejs')
